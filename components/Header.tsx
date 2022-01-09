4772e10ddcb6c710c0c18f984fd2f015e6976373
Rayan Kazi <rayan.quack.you@gmail.com>
2022-01-09 11:55:50 -0700

    completed header

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import type { NextComponentType } from "next";
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Header: NextComponentType = () => {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Trending" Icon={LightningBoltIcon} />
+        <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Collections" Icon={CollectionIcon} />
+        <HeaderItem title="Search" Icon={SearchIcon} />
+        <HeaderItem title="Account" Icon={UserIcon} />
+      </div>
+
+      <Image
+        src="/images/hulu.png"
+        width={200}
+        height={100}
+        className="object-contain"
+      />
+    </header>
+  );
+};
+
+export default Header;
